412fbc9168358f0049293545c35fe17d907a3122
Domz <70870457+gnobob@users.noreply.github.com>
2026-03-01 11:28:17 +0800

    Add files via upload

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+// Animation components
+export { Animate, FadeUp, FadeLeft, FadeRight, ZoomIn, FlipX, Reveal, SlideUp } from "./components/Animate";
+export { ScrollProgress, CursorGlow, SectionGlow } from "./components/MotionExtras";
+
+// Hooks
+export { useScrollAnimation } from "./hooks/useScrollAnimation";
+export { useMagnetic } from "./hooks/useMagnetic";
