580eb982daea349221e0faf80db2b74786854050
Domz <70870457+gnobob@users.noreply.github.com>
2026-03-01 12:03:52 +0800

    Integrate ScrollProgress and CursorGlow components
    Added global animation components for scroll progress and cursor glow.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { ScrollProgress, CursorGlow } from "@/components";
 
 const queryClient = new QueryClient();
 
@@ -13,10 +14,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      {/* Global animation effects */}
+      <ScrollProgress />
+      <CursorGlow />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
